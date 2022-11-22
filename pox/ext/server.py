79152f01e6973aecc0adbe5b69c3f37b50a4b958
bauti <jbrodriguez@fi.uba.ar>
2022-11-22 18:10:29 -0300

    change a.txt path to relative path

--- pox/ext/server.py
+++ pox/ext/server.py
@@ -2,7 +2,7 @@ import socket
 address = ('10.0.0.2', 3000)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(address)
-f = open('/home/julian/Desktop/Intro-2C22-G8-tp2/pox/ext/a.txt', 'w+')
+f = open('a.txt', 'w+')
 
 while True:
     data, addr = s.recvfrom(1024)
